4a2fc49dd6cdd9204846f9dbb361a06f53e13d5d
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-02 17:12:55 +0800

    rebuild frontend structure & lazy load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,54 @@
-import { useState } from "react";
-import "./CSS/App.css";
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+// import { useState, useEffect } from "react";
+import NavBar from "./components/Common/NavBar.tsx";
+// import NotFound from "./components/Common/NotFound.tsx";
+import HomePage from "./containers/HomePage.tsx";
+import Footer from "./components/Common/Footer.tsx";
+
+const LoginPage = lazy(() => import("./containers/User/LoginPage.tsx"));
+const ArticleMainPage = lazy(
+  () => import("./containers/Article/ArticleMainPage.tsx"),
+);
+const ForumMainPage = lazy(
+  () => import("./containers/Forum/ForumMainPage.tsx"),
+);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/LoginPage"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <LoginPage />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/Article"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ArticleMainPage />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/Forum"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ForumMainPage />
+            </Suspense>
+          }
+        />
+        <Footer />
+      </Routes>
     </>
   );
 }
