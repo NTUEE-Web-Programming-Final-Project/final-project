846dae57a3356a8895595d1939e10a78169f19ad
Bing-Wei, Chen <49435609+ChenBingWei1201@users.noreply.github.com>
2023-12-28 17:08:13 +0800

    solve conflicts (#78)
    * EDITOR-#54 [Backend] unlike resolver
    
    * EDITOR-#54 [Backend] unlike resolver

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -526,7 +526,7 @@ const Mutation = {
     if (!existingLike) {
       throw new Error("like not found!");
     }
-    
+
     // update liker
     const liker = await prisma.user.findFirst({
       where: {
--- backend/src/resolvers/Subscription.ts
+++ backend/src/resolvers/Subscription.ts
@@ -55,6 +55,7 @@ const Subscription = {
   ArticleCommentUpdated: {
     subscribe: () => pubsub.asyncIterator(["ARTICLECOMMENT_UPDATED"]),
   },
+  // Article End
   // ArticleComment End
 
   // ArticleLike Start
