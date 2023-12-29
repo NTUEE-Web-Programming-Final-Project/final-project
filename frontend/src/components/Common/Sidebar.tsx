d31ec621470d116dc3a415ee7d14cfa45545e7d8
Jackbear8868 <leo1314520528868@gmail.com>
2023-12-29 15:20:51 +0800

    [Frontend] EDITOR-#49 :art: Navbar + Sidebar + SearchBar

--- frontend/src/components/Common/Sidebar.tsx
+++ frontend/src/components/Common/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  const tags = [
+  const forums = [
     "Freshman",
     "Sophomore",
     "Junior",
@@ -10,6 +10,14 @@ const Sidebar = () => {
     "PhD",
     "Postdoctoral",
   ];
+  const articles = [
+    "Life",
+    "Evalutaion",
+    "ACG",
+    "Programing",
+    "Tech News",
+    "Recruit",
+  ];
 
   return (
     <>
@@ -17,19 +25,39 @@ const Sidebar = () => {
         <div className="flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
           <div className="overflow-y-auto overflow-x-hidden flex-grow">
             <ul className="flex flex-col py-4 space-y-1">
-              <li className="px-5">
+              <li className="py-1 px-5 bg-slate-100">
                 <div className="flex flex-row items-center h-8">
                   <div className="text-sm font-light tracking-wide text-gray-500">
-                    Menu
+                    Forum
                   </div>
                 </div>
               </li>
               <li></li>
-              {tags.map((tag) => {
+              {forums.map((tag) => {
                 return (
                   <li>
                     <Link
                       to={`/forum/${tag}`}
+                      className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
+                    >
+                      <span className="ml-2 text-sm tracking-wide truncate">{`${tag}`}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="py-1 px-5 bg-slate-100">
+                <div className="flex flex-row items-center h-8">
+                  <div className="text-sm font-light tracking-wide text-gray-500">
+                    Article
+                  </div>
+                </div>
+              </li>
+              <li></li>
+              {articles.map((tag) => {
+                return (
+                  <li>
+                    <Link
+                      to={`/article/${tag}`}
                       className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
                     >
                       <span className="ml-2 text-sm tracking-wide truncate">{`${tag}`}</span>
--- frontend/src/containers/HomePage.tsx
+++ frontend/src/containers/HomePage.tsx
@@ -1,7 +1,12 @@
+import Sidebar from "../components/Common/Sidebar";
+
 const HomePage = () => {
   return (
     <>
-      <h1 className="ml-3">Home Page</h1>
+      <div className="flex flex-row flex-auto">
+        <Sidebar />
+        <h1 className="ml-3">Home Page</h1>
+      </div>
     </>
   );
 };
