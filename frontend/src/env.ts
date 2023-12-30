b91a7df105141fc7065d342ebf79f656325f21cf
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-30 19:54:16 +0800

    EDITOR-#115 Deploy settings

---
+++ frontend/src/env.ts
@@ -0,0 +1,11 @@
+// private keys for firebase and magic link
+export const env = {
+  API_KEY: "AIzaSyDNpIep3qLJGUA5Q3RleedJ2m6VnNUSB7c",
+  AUTH_DOMAIN: "final-project-698b3.firebaseapp.com",
+  PROJECT_ID: "final-project-698b3",
+  STORAGE_BUCKET: "final-project-698b3.appspot.com",
+  MESSAGING_SENDER_ID: "538825364071",
+  APP_ID: "1:538825364071:web:247e1351ffe6391b81fac2",
+  MEASUREMENT_ID: "G-XY05F2LRW6",
+  MAGIC_PUBLIC_API_KEY: "pk_live_17C9A5ADAD69A7EA",
+};
