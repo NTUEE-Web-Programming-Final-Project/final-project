da5327c82aad2ddb51519ae1192de0ae260fac77
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-30 20:43:22 +0800

    EDITOR-#115 Deploy settings

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,44 +1,44 @@
 /* eslint-env node */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports", fixStyle: "separate-type-imports" },
-    ],
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  ignorePatterns: ["dist", "gql-generated"],
+  // env: {
+  //   browser: true,
+  //   es2021: true,
+  // },
+  // extends: [
+  //   "eslint:recommended",
+  //   "plugin:react/recommended",
+  //   "plugin:@typescript-eslint/recommended",
+  //   "plugin:react-hooks/recommended",
+  // ],
+  // overrides: [
+  //   {
+  //     env: {
+  //       node: true,
+  //     },
+  //     files: [".eslintrc.{js,cjs}"],
+  //     parserOptions: {
+  //       sourceType: "script",
+  //     },
+  //   },
+  // ],
+  // parser: "@typescript-eslint/parser",
+  // parserOptions: {
+  //   ecmaVersion: "latest",
+  //   sourceType: "module",
+  // },
+  // plugins: ["react", "@typescript-eslint"],
+  // rules: {
+  //   "react/react-in-jsx-scope": "off",
+  //   "no-console": ["warn", { allow: ["warn", "error"] }],
+  //   "@typescript-eslint/consistent-type-imports": [
+  //     "warn",
+  //     { prefer: "type-imports", fixStyle: "separate-type-imports" },
+  //   ],
+  // },
+  // settings: {
+  //   react: {
+  //     version: "detect",
+  //   },
+  // },
+  // ignorePatterns: ["dist", "gql-generated"],
 };
