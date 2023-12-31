b62496314aa010ff2c096ccacf743d3cba5c970c
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-31 09:36:05 +0800

    EDITOR-#129 fix errors to deploy

--- frontend/src/components/Common/Select.tsx
+++ frontend/src/components/Common/Select.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+// import * as React from "react";
 import type { Theme } from "@mui/material/styles";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
