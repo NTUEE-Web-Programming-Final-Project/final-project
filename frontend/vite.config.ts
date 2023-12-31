a88c16193bac85f0afa418ac5f7f99a1aedf0ae7
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-31 11:12:32 +0800

    EDITOR-#129 index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "./src/main.tsx",
-    },
-  },
 });
